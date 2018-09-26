74d1cb0ed0b0f160cd4d1331c5a630178cf12f2d
dangriffin13 <dangriffin13@gmail.com>
2018-09-25 21:49:34 -0400

    df setup

--- api_to_dataframe.py
+++ api_to_dataframe.py
@@ -7,12 +7,15 @@ import pandas as pd
 
 quandl_api_key = '&api_key=pWjXmxamqHYAMueDfPUE'
 
+#manual url - start date is throwing an error
 murl = f'https://www.quandl.com/api/v3/datasets/CME/CLH2018.json?start_date=2018-01-04&api_key=pWjXmxamqHYAMueDfPUE'
 
 r = requests.get(murl)
 
 dat = r.json()['dataset']
-
+arr = np.array(dat['data'])
+df = pd.DataFrame(arr)
+df.columns = dat['column_names']
 
 def quandl_api_wrapper(database_code, dataset_code): #CME, CLH2018
     quote_url = f'https://www.quandl.com/api/v3/datasets/{database_code}/{dataset_code}.json?'
@@ -27,5 +30,11 @@ def quandl_api_start_date(database_code, dataset_code, start_date): #CME, CLH201
         '{database_code}/{dataset_code}.json?start_date={start_date}'
 
     response = requests.get(quote_url + quandl_api_key)
+
+    murl = f'https://www.quandl.com/api/v3/datasets/CME/CLH2018.json?start_date=2018-01-04&api_key=pWjXmxamqHYAMueDfPUE'
+    print(response)
+    print(murl)
+    print(response==murl)
+    
     return response.json()
 
