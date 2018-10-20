55be412e5347b1df8eb3f06cdb1c419ab78940d8
dangriffin13 <dangriffin13@gmail.com>
2018-10-19 20:06:25 -0400

    f string adjust and start of unit tests

--- api_to_dataframe.py
+++ api_to_dataframe.py
@@ -8,14 +8,20 @@ import pandas as pd
 quandl_api_key = '&api_key=pWjXmxamqHYAMueDfPUE'
 
 #manual url - start date is throwing an error
-murl = f'https://www.quandl.com/api/v3/datasets/CME/CLH2018.json?start_date=2018-01-04&api_key=pWjXmxamqHYAMueDfPUE'
+# murl = f'https://www.quandl.com/api/v3/datasets/CME/CLH2018.json?start_date=2018-01-04&api_key=pWjXmxamqHYAMueDfPUE'
 
-r = requests.get(murl)
+# r = requests.get(murl)
 
-dat = r.json()['dataset']
-arr = np.array(dat['data'])
-df = pd.DataFrame(arr)
-df.columns = dat['column_names']
+# print('request done')
+
+# dat = r.json()['dataset']
+# arr = np.array(dat['data'])
+
+# print('arr created')
+
+# df = pd.DataFrame(arr)
+# df.columns = dat['column_names']
+# #df.sort_values(by='Date')
 
 def quandl_api_wrapper(database_code, dataset_code): #CME, CLH2018
     quote_url = f'https://www.quandl.com/api/v3/datasets/{database_code}/{dataset_code}.json?'
@@ -26,8 +32,7 @@ def quandl_api_wrapper(database_code, dataset_code): #CME, CLH2018
 
 def quandl_api_start_date(database_code, dataset_code, start_date): #CME, CLH2018
     #start date must be yyyy-mm-dd
-    quote_url = f'https://www.quandl.com/api/v3/datasets/' \
-        '{database_code}/{dataset_code}.json?start_date={start_date}'
+    quote_url = f'https://www.quandl.com/api/v3/datasets/{database_code}/{dataset_code}.json?start_date={start_date}'
 
     response = requests.get(quote_url + quandl_api_key)
 
@@ -35,6 +40,12 @@ def quandl_api_start_date(database_code, dataset_code, start_date): #CME, CLH201
     print(response)
     print(murl)
     print(response==murl)
-    
+
     return response.json()
 
+
+def url_test(database_code, dataset_code, start_date):
+    quote_url = f'https://www.quandl.com/api/v3/datasets/' \
+            f'{database_code}/{dataset_code}.json?start_date={start_date}'
+
+    return quote_url
