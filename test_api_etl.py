87a6348d83b8df55250c3e486b3b4f239d839a1b
dangriffin13 <dangriffin13@gmail.com>
2018-11-21 01:21:59 -0500

    unittests for methods added

--- test_api_etl.py
+++ test_api_etl.py
@@ -1,9 +1,22 @@
 import unittest
 import pandas as pd
+import requests
 from api_to_dataframe import quandl_api_wrapper
+
+call_url = 'https://www.quandl.com/api/v3/datasets/CME/CLH2018.json?start_date=2018-01-04&api_key=pWjXmxamqHYAMueDfPUE'
 
 class QuandlAPITest(unittest.Testcase):
 
     def setUp(self):
-        self.data = []
-        self.api_key = '&api_key=pWjXmxamqHYAMueDfPUE'
+        self.data = requests.get(call_url)
+
+
+    def test_api_connection:
+        self.assertTrue(self.data)
+
+
+    def test_df_columns:
+        self.assertEqual(quandl_to_df(self.data).columns.tolist(), ['Date', 'Open', 'High', 'Low', 'Last', 'Change', 'Settle', 'Volume', 'Previous Day Open Interest'])
+
+
+    def test_date_sorting:
