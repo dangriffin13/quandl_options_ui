ed9f954db1d528318a0f70e7aafd821db9502f99
dangriffin13 <dangriffin13@gmail.com>
2019-01-16 22:40:22 -0500

    metadata call

--- api_to_dataframe.py
+++ api_to_dataframe.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 quandl_api_key = '&api_key=pWjXmxamqHYAMueDfPUE'
 
+api_key_only = 'pWjXmxamqHYAMueDfPUE'
+
 #manual url - start date is throwing an error
 # murl = f'https://www.quandl.com/api/v3/datasets/CME/CLH2018.json?start_date=2018-01-04&api_key=pWjXmxamqHYAMueDfPUE'
 
@@ -23,11 +25,19 @@ quandl_api_key = '&api_key=pWjXmxamqHYAMueDfPUE'
 # df.columns = dat['column_names']
 # #df.sort_values(by='Date')
 
-def concatenate_api_key(url, api_key):
-    return url + quandl_api_key
-    
+def quandl_database_code_metadata_call(database_code, return_format='json'):
+    quote_url = f'https://www.quandl.com/api/v3/databases/{database_code}.{return_format}?api_key='
+    call_url = concatenate_api_key(quote_url + api_key_only)
 
-def quandl_api_wrapper(database_code, dataset_code): #CME, CLH2018
+    response = requests.get(call_url)
+    return response.json()
+
+
+def concatenate_api_key(url):
+    return url + quandl_api_key
+
+
+def quandl_api_wrapper(database_code, dataset_code): #CME, CLH2018  #CHRIS, ICE_T1
     quote_url = f'https://www.quandl.com/api/v3/datasets/{database_code}/{dataset_code}.json?'
  
     response = requests.get(quote_url + quandl_api_key)
@@ -50,15 +60,16 @@ def quandl_api_start_date(database_code, dataset_code, start_date): #CME, CLH201
 def url_test(database_code, dataset_code, start_date):
     quote_url = f'https://www.quandl.com/api/v3/datasets/' \
             f'{database_code}/{dataset_code}.json?start_date={start_date}'
-
     return quote_url
 
 def quandl_to_df(dat):
     dat = dat['dataset']
     df = pd.DataFrame(dat['data'])
     df.columns = dat['column_names']
-    df.sort_values(by='Date')
     return df
+
+def sort_df_by_date(df):
+    return df.sort_values(by='Date')    
 
 
 if __name__ == '__main__':
--- test_api_etl.py
+++ test_api_etl.py
@@ -3,22 +3,30 @@ import pandas as pd
 import requests
 from api_to_dataframe import quandl_api_wrapper
 
-call_url = 'https://www.quandl.com/api/v3/datasets/CME/CLH2018.json?start_date=2018-01-04&api_key=pWjXmxamqHYAMueDfPUE'
+data_call_url = 'https://www.quandl.com/api/v3/datasets/CME/CLH2018.json?start_date=2018-01-04&api_key=pWjXmxamqHYAMueDfPUE'
+
+
 
 class QuandlAPITest(unittest.Testcase):
 
     def setUp(self):
-        self.data = requests.get(call_url)
+        self.data = requests.get(data_call_url)
+        self.db_metadata = requests.get(db_call_url)
     
 
     def test_api_connection:
         self.assertTrue(self.data)
 
 
-    def test_df_columns:
+    def test_metadata_call:
+        self.assetTrue(self.db_metadata)
+
+
+    def test_df_column_headings:
         self.assertEqual(quandl_to_df(self.data).columns.tolist(), ['Date', 'Open', 'High', 'Low', 'Last', 'Change', 'Settle', 'Volume', 'Previous Day Open Interest'])
 
 
     def test_date_sorting:
-        df = quandl_to_df(self.data)
-        self.assertEqual(df.iloc[31]['Date'], '2018-01-04')
+        df = sort_df_by_date(quandl_to_df(self.data))
+        self.assertEqual(df.iloc[-1]['Date'], '2018-01-04')
+
