9ee3e3ea93da48f9e05e42964d7829ed62150134
dangriffin13 <dangriffin13@gmail.com>
2019-03-14 21:41:33 -0400

    adding some command line options in a view file

---
+++ view.py
@@ -0,0 +1,40 @@
+
+
+welcome_screen():
+    print('Welcome to the terminal line menu for the Quandl data')
+
+
+
+home_menu():
+    print('1. View List of Databases')
+    print('2. View Databases by Category')
+    print('3. Set Favorites')
+    print('4. View Dataset using Code')
+    selection = input('Select your choice: ')
+
+
+
+view_list_of_database():
+    for row in df:
+        print(df.iloc[row]['Name'], df.iloc[row]['Code'])
+    print('Press \'esc\' to return to Main Menu. \n\
+            Enter the database code to view more info.')
+    selection = input()
+
+
+view_list_of_databases_by_category():
+    pass
+
+
+set_favorites():
+    pass
+
+
+view_dataset_using_code():
+    print('Enter the database code (not the dataset code): ')
+    db_code = input()
+    print('Enter the dataset code: ')
+    dataset_code = input()
+
+    return db_code, dataset_code
+
