0cbf60f3b0f01325827124c772fe774e50c2a0b1
dangriffin13 <dangriffin13@gmail.com>
2019-03-24 19:14:21 -0400

    unit tests for db operations

---
+++ test_dao.py
@@ -0,0 +1,13 @@
+import unittest
+import pandas as pd
+import psycopg2
+import data_access_object as dao
+
+conn = psycopg2.connect(user='danielgriffin', password='', 
+                        database='quandl_ui', host='localhost')
+
+
+class QuandlDAOTest(unittest.Testcase):
+
+    def setUp(self):
+        self.connection = conn
