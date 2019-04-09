48d5bca51257c5f1beea90ddf98cfd8ed38bf646
dangriffin13 <dangriffin13@gmail.com>
2019-04-08 23:30:31 -0400

    setting up test cases for db interactions

--- test_dao.py
+++ test_dao.py
@@ -2,9 +2,12 @@ import unittest
 import pandas as pd
 import psycopg2
 import data_access_object as dao
+import api_to_dataframe as api
 
 conn = psycopg2.connect(user='danielgriffin', password='', 
                         database='quandl_ui', host='localhost')
+
+
 
 
 class QuandlDAOTest(unittest.Testcase):
